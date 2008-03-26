39b5b9badb505faa9c5ab7d6af0611b5c77241db
francois.rouaix <francois.rouaix@0879ad58-e641-0410-9a37-451499ef21ad>
2008-03-26 14:01:50 +0000

    Sync

--- support/kvc.ml
+++ support/kvc.ml
@@ -1,3 +1,7 @@
 (* Key Value coding *)
-external get : 'a Objc.id -> string -> [`NSValue] Objc.id = "camlkvc_get"
-external set : 'a Objc.id -> string -> [`NSValue] Objc.id -> unit = "camlkvc_set"
+(* same primitive with two types - obviously not too safe *)
+external get_value : 'a Objc.id -> string -> [`NSValue] Objc.id = "camlkvc_get"
+external get_object : 'a Objc.id -> string -> [`NSObject] Objc.id = "camlkvc_get"
+
+external set_value : 'a Objc.id -> string -> [`NSValue] Objc.id -> unit = "camlkvc_set"
+external set_object : 'a Objc.id -> string -> [`NSObject] Objc.id -> unit = "camlkvc_set"
